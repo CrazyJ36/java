3aa571094191e208c56e8334fc31d049334b2a9f
CrazyJ36 <crazy_j36@hotmail.com>
2018-02-01 02:15:43 +0000

    smaller code

--- Date/theDate.java
+++ Date/theDate.java
@@ -1,8 +1,6 @@
 import java.util.Date;
-
 public class theDate {
-    public static void main(String args[]) {
-        Date d = new Date();
-        System.out.println(d.toString());
-    }
+  public static void main(String args[]) {
+    System.out.println(new Date());
+  }
 }
