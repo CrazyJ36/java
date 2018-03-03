a12717775551af5f66cf4555eb9eea8174d466d2
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-03 09:57:12 +0000

    include string array example

---
+++ strArray/strArray.java
@@ -0,0 +1,8 @@
+public class strArray {
+  public static void main(String[] args) {
+    String[] arr = {"hi", "world", "all"};
+    System.out.println(arr[0]);
+    System.out.println(arr[2]);
+  }
+}
+
