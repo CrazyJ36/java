73e3b51065579eb1472297f2f29ae2634a8fc0f1
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-19 17:25:27 +0000

    add example of double math, add console input example

---
+++ doubleEx/doubleEx.java
@@ -0,0 +1,6 @@
+public class doubleEx {
+    public static void main(String[] args) {
+        double dbl = 45.2;
+	System.out.println(dbl);
+    }
+}
---
+++ userInput/ex2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+	System.out.print("type: ");
+        Scanner input = new Scanner(System.in);
+	System.out.println("you typed: " + input.nextLine());
+    }
+}
