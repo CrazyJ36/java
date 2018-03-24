a653fab51d4b2a84eee922cfbe0acfd688eb335f
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-24 06:17:05 +0000

    organize

---
+++ examples/showArrayLength/showArrayLength.java
@@ -0,0 +1,8 @@
+public class showArrayLength {
+    public static void main(String[] args) {
+        String[] arr = {"hi","tj","kennedy"};
+	System.out.println(arr.length);
+ 	System.out.println("there a that many items:");
+        System.out.println(arr[0] + arr[1] + arr[2]);
+   }
+}
--- stringAsVar/stringAsVar.java
+++
@@ -1,6 +0,0 @@
-class stringAsVar {
-    public static void main(String args[]) {
-        String x = "hi";
-        System.out.println(x);
-    }
-}
