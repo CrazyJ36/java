5bba2c8504cea440c73480da2de622dcab2146fe
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-28 19:57:44 -0400

    arrayToString

---
+++ examples/arrayToString/listArray2.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class listArray2 {
+
+  public static void main(String[] args) {
+    String[] array = new String[]{"hi","tj"};
+    String arrayText = array[0].toString();
+    System.out.println(arrayText);
+  }
+}
