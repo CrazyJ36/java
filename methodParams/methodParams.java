e157b90a99c8330d6dd8146b22b2cba6adc4c36f
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-29 16:40:41 -0400

    all projects to main dir, method parameters

---
+++ methodParams/methodParams.java
@@ -0,0 +1,9 @@
+public class methodParams {
+    public static void main() {
+	sayHello("you");
+	sayHello("me");
+    }
+    public void sayHello(String name) {
+	    System.out.println("Hello" + name);
+    }
+}
