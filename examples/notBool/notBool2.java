9a389e4111e35dfaa14cfbb180eb29fbe3f38019
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-29 14:13:10 -0400

    easy was to say 'if (thing)  is not'

---
+++ examples/notBool/notBool2.java
@@ -0,0 +1,21 @@
+public class notBool2 {
+    public static void(String[] args) {
+        boolean somethingTrue = true;
+/* somethingTrue can be evaluated for trueness (is) as:
+if (somethingTrue == true) {//..}
+or
+if (somethingTrue) {//..} as it was already true when it was
+defined as a true variable.
+*/
+/* evaluating for falseness (is not somethingTrue):
+if (somethingTrue != true) {//..}
+or
+if (!somesthingTrue) {//..}
+*/
+	if (somethingTrue) {
+	    System.out.println("true is true");
+	} else (!somethingTrue) {
+	      System.out.println("true is false");
+	}
+   }
+}
