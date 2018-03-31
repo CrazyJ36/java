e9ccefbfc0e1feb866ee66d6c824bf6524a65c52
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-31 12:58:28 -0400

    add timer

---
+++ timer/timer.java
@@ -0,0 +1,16 @@
+package com.crazyj36;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        int x = 0;
+        while (x < 10) {
+            x++;
+            if (x == 8) {
+                System.out.print("Almost Done: ");
+            }
+            System.out.println(x);
+            Thread.sleep(1000);
+        }
+    }
+}
