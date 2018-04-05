e752a377d02e829ab9c4c6041eced90254cf7ba7
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-05 03:41:04 +0000

    learned all access level modifiers

--- accessLevelModifiers/publicClass.java
+++ accessLevelModifiers/publicClass.java
@@ -1,12 +1,14 @@
 /* This example demonstrates what happens when
-no access modifier is defined for the files' class.
+no access modifier is defined for the class file.
 (line 1, class Name {})
 Java then defaults to the default class access level:
 package-private,
 Making the class visible to only its own package.
 Similar to non-open-source, this make this particular
-programs functionality not available to add to other
+programs functionality(classes, attributes, and methods)
+not available to add or usein other
 packages with 'javac --classpath'.
+
 No modifier defined or package private =
 granted access level: class, package
 denied access: subclasses, world
