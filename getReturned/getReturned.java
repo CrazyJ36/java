9aa14fc3d536313019935233cf5e03ef6cb3db50
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-06 16:52:05 +0000

    work on getters/setters', just like using class objects

---
+++ getReturned/getReturned.java
@@ -0,0 +1,10 @@
+public class getReturned {
+    public static String getText() {
+    	String text = "hi";
+	return this.text;
+    }
+
+    public static void main(String[] args) {
+        getText();
+    }
+}
