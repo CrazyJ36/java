a4240ff1f79509cb41cdcc9452a8b0aa4c2ae99e
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-17 18:35:14 -0400

    example methodParams using two variables

--- methodParams/methodParams.java
+++ methodParams/methodParams.java
@@ -2,8 +2,12 @@ public class methodParams {
     public static void main(String[] args) {
 	sayHello("you");
 	sayHello("me");
+        twoParams("text", 2);
     }
     public static void sayHello(String name) {
-	    System.out.println("Hello" + name);
+	System.out.println("Hello" + name);
+    }
+    public static void twoParams(String txt, int num) {
+	System.out.println(txt +  num);
     }
 }
