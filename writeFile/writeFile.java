a4924861baa92d9e9ba95ae28f2ac0d1858c9445
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-30 16:17:14 -0400

    writing a file

---
+++ writeFile/writeFile.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class writeFile {
+    public static void main(String[] args) throws IOException {
+        // Instantiate A file with name 'file'. The filename will be 'file.txt' 
+        File file = new File("file.txt");
+        // Call new FileWriter with name writer to write the file 'file'
+        FileWriter writer = new FileWriter(file);
+        // write the file with some text.
+        writer.write("Content text\n");
+        // close the FileWriter as to not continue using it's resources.
+        writer.close();
+    }
+}
