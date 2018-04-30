a9f19aad4fedfdb1ed9283c388816e9d528acf96
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-29 22:43:41 -0400

    practice packages

---
+++ package/package1/com/crazyj36/packageexample/myClass.java
@@ -0,0 +1,7 @@
+package com.crazyj36.packageexample;
+
+public class myClass {
+    public static void doStuff() {
+        System.out.println("myClass ran");
+    }
+}
--- package/package1/com/crazyj36/packageexample/packageexample.java
+++ package/package1/com/crazyj36/packageexample/packageexample.java
@@ -2,6 +2,8 @@ package com.crazyj36.packageexample;
 
 class packageexample {
     public static void main(String args[]) {
-        System.out.println("Package ran");
+        System.out.println("Package running main using other class files.");
+        myClass stuff = new myClass();
+        stuff.doStuff();
     }
 }
