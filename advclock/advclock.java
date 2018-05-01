e50b428662527a86d2e67a3cc67808b2c1dbdcb0
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-30 22:28:00 -0400

    advclcock improvements, supports month

--- advclock/advclock.java
+++ advclock/advclock.java
@@ -1,34 +1,39 @@
 import java.time.*;
+import java.time.format.*;
+
 public class advclock {
     public static void main(String args[]) {
-        ZonedDateTime timeZone = ZonedDateTime.now();
-        LocalDate monthDay = LocalDate.from(LocalDateTime.now());
-            // Argument Usage Checks
         if (args.length < 1) {
-            System.out.println("No arguments given, try \"java advclock -h.\"");
+            System.out.println("No arguments given, try 'java advclock help'.");
             System.exit(0);
-        } else if (args[0].equals("-h") || args[0].equals("--help")) {
+        } else if (args[0].equals("help")) {
             help();
-           //Time Related Tasks
-        } else if (args[0].equals("timezone")) {
-            System.out.println(timeZone.getZone());
-        }
-          else if (args[0].equals("number")) {
-            System.out.println(monthDay.getDayOfMonth());
+        // Tasks
+        } else if (args[0].equals("zone")) {
+            System.out.println(ZonedDateTime.now().getZone());
+        } else if (args[0].equals("num")) {
+            System.out.println(MonthDay.now().getDayOfMonth());
         } else if (args[0].equals("clock")) {
-            System.out.println(LocalTime.now());
+            LocalTime utime = LocalTime.now();
+            DateTimeFormatter time = DateTimeFormatter.ofPattern("h"+":"+"m"+" "+"a");
+            String mtime = utime.format(time);
+            System.out.println(mtime);
+        } else if (args[0].equals("month")) {
+            String month = YearMonth.now().getMonth().toString().toLowerCase();
+            char fl = month.charAt(0);
+            char flu = Character.toUpperCase(fl);
+            String fmonth = month.replace(fl, flu);
+            System.out.println(fmonth);
         } else {
-	    help();
-	}
+        help();
+        }
     }
     public static void help() {
-        System.out.println("Usable arguments are:\nclock   \t- ticking clock in seconds");
-        System.out.println("timezone\t- show your timezone.\nnumber  \t- show numerical day of the month.");
-        System.out.println("-h, --help\t- for this help text"); 
+        System.out.println("To show useful info, usable arguments are:\n");
+        System.out.println("clock    \t- current time of day.");
+        System.out.println("zone     \t- your timezone.");
+        System.out.println("num      \t- numerical day of the month.");
+        System.out.println("month    \t- month of the year.");
+        System.out.println("help     \t- this help.");
     }
 }
-
-/* NOTES:
-more good arguments to add (find how to use in java.time api):
-
-month */
