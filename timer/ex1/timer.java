0f42530c15fce12a7ea75fa60710b9c3fbac8c33
CrazyJ36 <crazy_j36@hotmail.com>
2018-08-21 03:28:24 -0400

    u

--- timer/ex1/timer.java
+++ timer/ex1/timer.java
@@ -1,14 +1,20 @@
 public class timer {
-
     public static void main(String[] args) throws InterruptedException {
         int x = 0;
         while (x < 10) {
             x++;
-            if (x == 8) {
-                System.out.print("Almost Done: ");
-            }
-            System.out.println(x);
+	    if (x == 1) {
+		    System.out.println(x + " Started");
+	    }
+	    else if (x == 5) {
+		    System.out.println(x + " Halfway");
+	    }
+	    else {
+		    System.out.println(x);
+	    }
             Thread.sleep(1000);
         }
+	System.out.println("Done.");
+	System.exit(0);
     }
 }
