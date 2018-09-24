c9ca0f0ecdd13cd3f937509045511003efdcb946
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-24 19:15:58 -0400

    another jframe test

---
+++ graphics/jframeTest2/jframeTest2.java
@@ -0,0 +1,32 @@
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+public class jframeTest2 extends Canvas {
+
+    public static void main(String[] args) {
+        JFrame jframe = new JFrame("App"); // Window with title
+        Canvas canvas = new jframeTest2(); // Put this class into Canvas Type
+        canvas.setSize(300, 200);
+        jframe.add(canvas);
+        jframe.pack(); // Auto pack frame
+        jframe.setVisible(true);
+    }
+    /* A paint method is required by canvas,
+     * is always running in Canvas
+     
+     * Drawing uses javas coordinates:
+     * x is top horizontal line,
+     * y is left vertical line.
+     * x=0, y=0 is top left.
+     * Multiple points are often used [start(x,y); end(x,y)]
+     */
+    public void paint(Graphics graphics) {
+    	graphics.drawString(mString(), 20, 20);
+    	graphics.drawLine(20, 30, 60, 30);
+    }
+
+    public String mString() {
+    	return "five";
+    }
+}
