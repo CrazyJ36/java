2ea31773c986966ecc8b345692bb3ab5b67967de
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-25 03:02:05 -0400

    boolean variable example

---
+++ newBoolean/newBoolean.java
@@ -0,0 +1,10 @@
+class newBoolean {
+    public static void main(String[] args) {
+        boolean check = false; // can make any boolean
+        if (check) { // if implies true
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
