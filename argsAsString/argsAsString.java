631319b60f0c0a1d11f8029c1fcd9d20b7590b7e
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-26 18:51:16 -0400

    start using gradle

--- argsAsString/argsAsString.java
+++
@@ -1,7 +0,0 @@
-public class argsAsString {
-  public static void main(String[] args) {
-    String myInput = args[0];
-    System.out.println(myInput);
-    System.exit(0);
-  }
-}
---
+++ argsAsString/src/main/java/com/crazyj36/argsAsString/argsAsString.java
@@ -0,0 +1,9 @@
+package com.crazyj36.argsAsString;
+
+public class argsAsString {
+  public static void main(String[] args) {
+    String myInput = args[0];
+    System.out.println(myInput);
+    System.exit(0);
+  }
+}
