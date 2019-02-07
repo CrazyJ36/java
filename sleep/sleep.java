39e3fd3d528627be6c0db72931370daab4ecf34a
CrazyJ36 <crazy_j36@hotmail.com>
2019-02-07 12:33:30 -0500

    u

--- sleep/sleep.java
+++ sleep/sleep.java
@@ -1,19 +1,14 @@
 public class sleep {
-    public static void main(String args[])
-        throws InterruptedException {
-        String importantInfo[] = {
-            "Mares eat oats",
-            "Does eat oats",
-            "Little lambs eat ivy",
-            "A kid might eat ivy"
-        };
-        for (int i = 0;
-             i < importantInfo.length;
-             i++) {
-            //Pause for 4 seconds
-            Thread.sleep(4000);
-            //Print a message
-            System.out.println(importantInfo[i]);
+	
+    public static void main(String args[]) throws InterruptedException {
+		
+        char chars[] = {'A','B','C','D'};
+		int i = 0;
+        while(i < chars.length) {
+            System.out.print(chars[i]);
+			i++;
+			Thread.sleep(1000);
         }
+		System.exit(0);
     }
 }
