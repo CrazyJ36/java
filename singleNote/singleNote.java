48e94ac71f7675327c4b6811937d2a334cef50d8
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-15 03:55:57 -0400

    u

--- singleNote/singleNote.java
+++ singleNote/singleNote.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 
 public class singleNote {
-	
     public static void main(String[] args) throws IOException {
         File file = new File("note.txt");
         final FileWriter writer = new FileWriter(file);
-        try { 
+        try {
             writer.append("Latest: " + args[0].toString() + "\n");
-            
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Empty note. Enter any 'text' to put into note.txt");
         }
