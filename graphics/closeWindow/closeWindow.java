5a55fc6bdf3a7bfa76b82837888869bb1252829a
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-09 05:26:31 -0400

    graphics/mButton: improved for general use

--- graphics/closeWindow/closeWindow.java
+++ graphics/closeWindow/closeWindow.java
@@ -11,15 +11,13 @@ public class closeWindow extends Canvas {
     public static void main(String[] args) {
         closeWindow closeWindowVar = new closeWindow();
 		JFrame jFrame = new JFrame();
-		
 		// Listen at window for event, this System.exit(), ending cmdline jre
-		jFrame.addWindowListener(new WindowAdapter() { 
+		jFrame.addWindowListener(new WindowAdapter() {
 		    public void windowClosing(WindowEvent windowEvent){ // windowClosing() is defined in api to be mapped to close button on window
                 System.out.println("Close button clicked\nExiting..");
 				System.exit(0);
             }
         });
-		
         jFrame.add(closeWindowVar);
         jFrame.setSize(100,100);
         jFrame.setVisible(true);
--- graphics/mButton/mButton.java
+++ graphics/mButton/mButton.java
@@ -6,7 +6,7 @@ public class mButton {
     JFrame jFrame = new JFrame("myButton");
     private Label statusLabel;
     private Panel controlPanel;
-    
+
 	public mButton(){
         prepareGUI();
 	}
@@ -14,34 +14,42 @@ public class mButton {
 	public static void main(String[] args){
 		mButton  mButtonVar = new mButton();
 		mButtonVar.showEventDemo();
-	}
+
+    }
 
 	private void prepareGUI(){
 		jFrame.setSize(400,400);
-		statusLabel = new Label();       
+		statusLabel = new Label();
 		statusLabel.setSize(100,80);
+        statusLabel.setText("Click button");
 		controlPanel = new Panel();
 		controlPanel.setLayout(new FlowLayout());
 		jFrame.add(statusLabel);
 		jFrame.add(controlPanel);
-		jFrame.setVisible(true);  
+		jFrame.setVisible(true);
+        jFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){ // windowClosing() is defined in api to be mapped to close button on window
+                System.out.println("Close button clicked\nExiting..");
+                System.exit(0);
+            }
+        });
 	}
 
 	private void showEventDemo(){
 		Button okButton = new Button("OK");
 		okButton.setSize(50,50);
 		okButton.setActionCommand("OK");
-		okButton.addActionListener(new ButtonClickListener()); 
+		okButton.addActionListener(new ButtonClickListener());
 		controlPanel.add(okButton);
 		jFrame.setVisible(true);
 	}
 
 	private class ButtonClickListener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			String command = e.getActionCommand();  
+			String command = e.getActionCommand();
 			if( command.equals( "OK" ))  {
-				statusLabel.setText("Ok Button clicked.");
-			}	
-		}		
+				statusLabel.setText("Ok Clicked.");
+			}
+		}
 	}
 }
