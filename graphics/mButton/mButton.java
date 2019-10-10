db5a6e7eeb33b92039052396b8104571a85e0542
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-10 17:51:06 -0400

    graphics/mButton: lighten code

--- graphics/mButton/mButton.java
+++ graphics/mButton/mButton.java
@@ -1,36 +1,25 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class mButton {
-
 	public static void main(String[] args){
         JFrame frame = new JFrame("Button App");
 		frame.setSize(400,300);
-		Label label = new Label();
-		label.setSize(20,40);
-        label.setText("Click button");
+        frame.setDefaultCloseOperation(3);
+		Label label = new Label("Click the button.");
         Button button = new Button("OK");
-        button.setSize(50, 25);
-		button.setActionCommand("OK");
+        button.setSize(25, 25);
 		button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
-                if (actionEvent.getActionCommand().equals("OK")) {
-                    label.setText("Ok Clicked.");
-                    System.out.println("Ok clicked in frame.");
-                }
+                label.setText("Ok Clicked.");
+                System.out.println("Button clicked on frame.");
             }
         });
         frame.add(button);
 		frame.add(label);
 		frame.setVisible(true);
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent windowEvent){ // windowClosing() in api mapped to close button.
-                System.out.println("Close");
-                System.exit(0);
-            }
-        });
-
 	}
 
 }
