431af0b342fab1ad62bce656dd42ffeef819e883
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-11 13:35:03 -0400

    fix grapics/basicSwing app for general use

--- graphics/basicSwing/basicSwing.java
+++ graphics/basicSwing/basicSwing.java
@@ -12,10 +12,9 @@ public class basicSwing {
     public static void setupHelloUi() {
         frame = new JFrame("Hello App");
         frame.setSize(200, 100);
-        frame.setDefaultCloseOperation(2); // Dispose on close
+        frame.setDefaultCloseOperation(3); // dispose_on_close(2) causes exit() if no other windows are active
         Label label = new Label("Hello World");
         frame.add(label);
-        frame.setVisible(true);
     }
 
     public static void setupBtnUi() {
@@ -33,7 +32,6 @@ public class basicSwing {
         });
         frame.add(button);
 		frame.add(label);
-        frame.setVisible(true);
     }
 
     public static void showUi() {
@@ -44,11 +42,12 @@ public class basicSwing {
         System.out.println("Enter 'hello', or 'button': ");
 	    Scanner scanner = new Scanner(System.in);
         String choice = scanner.nextLine();
-
-        if (choice == "hello") {
+        if (choice.equals("hello")) {
             setupHelloUi();
-        } else if (choice == "button") {
+            showUi();
+        } else if (choice.equals("button")) {
             setupBtnUi();
+            showUi();
         } else {
             System.out.println("Got Neither.");
             System.exit(0);
