d19b61c27dcccfb2bf6b8fdbc5d747036e104d5a
CrazyJ36 <crazy_j36@hotmail.com>
2019-12-29 00:22:35 -0500

    u

--- webContent/webContent.java
+++ webContent/webContent.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class webContent {
     public static void main(String[] args) throws Exception {
-        URL site = new URL("https://www.york.ac.uk/teaching/cws/wws/webpage1.html");
+        URL site = new URL("http://192.168.1.166/");
         BufferedReader in = new BufferedReader(
         new InputStreamReader(site.openStream()));
         String inputLine;
