545cdce19898eac95866293feca48865a7745020
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-27 17:46:23 -0500

    practice for each loop

--- forLoopEnhanced/forLoopEnhanced.java
+++ forLoopEnhanced/forLoopEnhanced.java
@@ -1,9 +1,6 @@
 class forLoopEnhanced {
     public static void main(String[] args) {
-/* Enhanced for loop(for each loop) is an easy way to
-   traverse elements in arrays.
-   less code like this reduces bugs with fewer lines
-   of code and makes source easy to read.
+/* Enhanced for loop(or 'for each' loop), traverses elements in arrays.
 
    write as:
    for (type varName: array) {
