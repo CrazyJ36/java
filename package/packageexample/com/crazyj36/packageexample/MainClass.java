4d7204ceafc99837ac94a0cfdae09558503c0323
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-30 15:57:03 -0500

    packaging and import practice

--- package/packageexample/com/crazyj36/packageexample/MainClass.java
+++ package/packageexample/com/crazyj36/packageexample/MainClass.java
@@ -5,6 +5,11 @@ package com.crazyj36.packageexample;
 /* You could import the MainClass1, then use it's methods, though this is not needed as it's found in the package. */
 // import com.crazyj36.packageexample.MyClass1;
 
+/* If you want to use classes that are not in you package, or in another directory,
+   just import them compile your program that uses them by telling javac where those classes are
+   by the --classpath flag when compiling with javac.
+*/
+
 class MainClass {
     public static void main(String args[]) {
         System.out.println("MainClass().main()");
