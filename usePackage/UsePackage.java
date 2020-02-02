d80e16d41b0398c51ecb88d20a59329789f5c90b
CrazyJ36 <crazy_j36@hotmail.com>
2020-02-02 01:11:59 -0500

    use your other packages in programs

---
+++ usePackage/UsePackage.java
@@ -0,0 +1,11 @@
+import com.crazyj36.myclasses.DummyString;
+
+public class UsePackage {
+
+    public static void main(String[] args) {
+        System.out.println("getting dummystring from package myclasses");
+        System.out.println(new DummyString().dummyString());
+        System.out.println("main done...");
+    }
+
+}
---
+++ usePackage/com/crazyj36/myclasses/DummyString.java
@@ -0,0 +1,9 @@
+package com.crazyj36.myclasses;
+
+public class DummyString {
+
+    public String dummyString() {
+        return "dummy string";
+    }
+
+}
